7aa211fc6d1d1465e014757f81bad044729236d6
juanmiguel549-rgb <juanmiguel549@gmail.com>
2026-04-27 13:30:59 -0600

    Actualizar Service ID de EmailJS

--- src/lib/emailConfig.ts
+++ src/lib/emailConfig.ts
@@ -1,6 +1,6 @@
 export const EMAIL_CONFIG = {
   // Reemplaza estos valores con los que te de EmailJS
-  SERVICE_ID: 'service_fgkpl3p',
+  SERVICE_ID: 'service_cgsez6d',
   TEMPLATE_ID: 'template_om3i3q3',
   PUBLIC_KEY: 'tsQBgvdbjQfnAloIb',
 };
